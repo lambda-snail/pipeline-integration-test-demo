4a215f02a7974c6be411fd58d14aa690b1a4143e
Niclas Blomberg <79707893+lambda-snail@users.noreply.github.com>
2022-11-02 09:35:11 +0100

    chore: change to file-scope namespace

--- BlobStorageIntegration/BlobContainerManager.cs
+++ BlobStorageIntegration/BlobContainerManager.cs
@@ -5,56 +5,54 @@ using Microsoft.WindowsAzure.Storage.Blob;
 
 #nullable enable
 
-namespace BlobStorageIntegration
+namespace BlobStorageIntegration;
+
+/// <summary>
+/// Encapsulates operations and functionality related to blob storage in Azure.
+/// </summary>
+public class BlobContainerManager
 {
-    /// <summary>
-    /// Encapsulates operations and functionality related to blob storage in Azure.
-    /// </summary>
-    public class BlobContainerManager
+    string? _connectionString;
+    CloudBlobContainer? _container;
+
+    public bool IsInitialized => _container is not null;
+
+    public BlobContainerManager(string connectionString)
     {
-        string? _connectionString;
-        CloudBlobContainer? _container;
+        _connectionString = connectionString ?? throw new ArgumentNullException("Error: Connection string must not be null.");
+    }
 
-        public bool IsInitialized => _container is not null;
-
-        public BlobContainerManager(string connectionString)
+    public async Task InitBlobContainerManager(string containerName)
+    {
+        CloudStorageAccount? stAccount = null;
+        if (CloudStorageAccount.TryParse(_connectionString, out stAccount))
         {
-            _connectionString = connectionString ?? throw new ArgumentNullException("Error: Connection string must not be null.");
-        }
+            CloudBlobClient client = stAccount.CreateCloudBlobClient();
+            _container = client.GetContainerReference(containerName);
 
-        public async Task InitBlobContainerManager(string containerName)
-        {
-            CloudStorageAccount? stAccount = null;
-            if (CloudStorageAccount.TryParse(_connectionString, out stAccount))
-            {
-                CloudBlobClient client = stAccount.CreateCloudBlobClient();
-                _container = client.GetContainerReference(containerName);
-
-                await _container.CreateIfNotExistsAsync();
-            }
-        }
-
-        public Task SaveBlobAsync(string blobName, string content)
-        {
-            EnsureInitialized();
-            CloudBlockBlob blob = _container!.GetBlockBlobReference(blobName);
-            return blob.UploadTextAsync(content);
-        }
-
-        public Task<string> ReadBlobAsync(string blobName)
-        {
-            EnsureInitialized();
-            CloudBlockBlob blob = _container!.GetBlockBlobReference(blobName);
-            return blob.DownloadTextAsync();
-        }
-
-        private void EnsureInitialized()
-        {
-            if (!IsInitialized) 
-            { 
-                throw new InvalidOperationException("Error: Attempt to operate on an uninitialized blob container manager."); 
-            }
+            await _container.CreateIfNotExistsAsync();
         }
     }
-}
 
+    public Task SaveBlobAsync(string blobName, string content)
+    {
+        EnsureInitialized();
+        CloudBlockBlob blob = _container!.GetBlockBlobReference(blobName);
+        return blob.UploadTextAsync(content);
+    }
+
+    public Task<string> ReadBlobAsync(string blobName)
+    {
+        EnsureInitialized();
+        CloudBlockBlob blob = _container!.GetBlockBlobReference(blobName);
+        return blob.DownloadTextAsync();
+    }
+
+    private void EnsureInitialized()
+    {
+        if (!IsInitialized) 
+        { 
+            throw new InvalidOperationException("Error: Attempt to operate on an uninitialized blob container manager."); 
+        }
+    }
+    }
--- BlobStorageIntegration/SaveFileFunction.cs
+++ BlobStorageIntegration/SaveFileFunction.cs
@@ -12,44 +12,43 @@ using Microsoft.OpenApi.Models;
 
 #nullable enable
 
-namespace BlobStorageIntegration
+namespace BlobStorageIntegration;
+
+public class SaveFileFunction
 {
-    public class SaveFileFunction
+    private readonly ILogger<SaveFileFunction> _logger;
+    private readonly BlobContainerManager _blobManager;
+
+    public SaveFileFunction(ILogger<SaveFileFunction> log)
     {
-        private readonly ILogger<SaveFileFunction> _logger;
-        private readonly BlobContainerManager _blobManager;
+        _logger = log;
+        _blobManager = new(Environment.GetEnvironmentVariable("AzureWebJobsStorage")!);
+    }
 
-        public SaveFileFunction(ILogger<SaveFileFunction> log)
+    [FunctionName("SaveFile")]
+    [OpenApiOperation(operationId: "Run", tags: new[] { "blob" })]
+    [OpenApiSecurity("function_key", SecuritySchemeType.ApiKey, Name = "code", In = OpenApiSecurityLocationType.Query)]
+    [OpenApiParameter(name: "containerName", In = ParameterLocation.Path, Required = true, Type = typeof(string), Description = "The name of the blob storage to save in.")]
+    [OpenApiParameter(name: "blobName", In = ParameterLocation.Path, Required = true, Type = typeof(string), Description = "The name of the blob to store.")]
+    [OpenApiRequestBody(contentType: "text/plain", bodyType: typeof(string), Description = "The content of the file.", Required = true)]
+    [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "application/json", bodyType: typeof(string), Description = "If the file was saved successfully.")]
+    public async Task<IActionResult> Run(
+        [HttpTrigger(AuthorizationLevel.Function, "post", Route = "containers/{containerName}/blobs/{blobName}")] HttpRequest req,
+        [FromRoute] string containerName,
+        [FromRoute] string blobName
+        )
+    {
+        _logger.LogInformation("Save {blob} to {storage}", blobName, containerName);
+
+        string content = await req.ReadAsStringAsync();
+        if(string.IsNullOrWhiteSpace(content))
         {
-            _logger = log;
-            _blobManager = new(Environment.GetEnvironmentVariable("AzureWebJobsStorage")!);
+            return new BadRequestResult();
         }
 
-        [FunctionName("SaveFile")]
-        [OpenApiOperation(operationId: "Run", tags: new[] { "blob" })]
-        [OpenApiSecurity("function_key", SecuritySchemeType.ApiKey, Name = "code", In = OpenApiSecurityLocationType.Query)]
-        [OpenApiParameter(name: "containerName", In = ParameterLocation.Path, Required = true, Type = typeof(string), Description = "The name of the blob storage to save in.")]
-        [OpenApiParameter(name: "blobName", In = ParameterLocation.Path, Required = true, Type = typeof(string), Description = "The name of the blob to store.")]
-        [OpenApiRequestBody(contentType: "text/plain", bodyType: typeof(string), Description = "The content of the file.", Required = true)]
-        [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "application/json", bodyType: typeof(string), Description = "If the file was saved successfully.")]
-        public async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "post", Route = "containers/{containerName}/blobs/{blobName}")] HttpRequest req,
-            [FromRoute] string containerName,
-            [FromRoute] string blobName
-            )
-        {
-            _logger.LogInformation("Save {blob} to {storage}", blobName, containerName);
+        await _blobManager.InitBlobContainerManager(containerName);
+        await _blobManager.SaveBlobAsync(blobName, content);
 
-            string content = await req.ReadAsStringAsync();
-            if(string.IsNullOrWhiteSpace(content))
-            {
-                return new BadRequestResult();
-            }
-
-            await _blobManager.InitBlobContainerManager(containerName);
-            await _blobManager.SaveBlobAsync(blobName, content);
-
-            return new OkResult();
-        }
+        return new OkResult();
     }
 }
